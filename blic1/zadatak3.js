f8f8c5118610276366f2aeeb3fd322d6af3ff9fa
Leonarda Banić <banic.leonarda2@gmail.com>
2022-10-11 08:28:40 +0200

    Rjeseni zadatak 3

--- blic1/zadatak3.js
+++ blic1/zadatak3.js
@@ -3,15 +3,12 @@ function usporedi(a, b) {
     if (typeof a === 'number' && typeof b === 'number') {
         return a - b;
     }
-    // check for num vs string
     if (typeof a === 'number' && typeof b === 'string') {
         return -1;
     }
-    // check for string vs num
     if (typeof a === 'string' && typeof b === 'number') {
         return 1;
     }
-    // check for string vs string
     if (typeof a === 'string' && typeof b === 'string') {
         if (a < b) return -1;
         else return 1;
